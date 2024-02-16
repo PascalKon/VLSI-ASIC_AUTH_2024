9eefd7379b1c4360e841fbc7ad86be821ecc065c
PascalKon <118291843+PascalKon@users.noreply.github.com>
2024-02-16 17:21:40 +0200

    Add files via upload

---
+++ Ex7/Step1-7/Ex7_Step1-7_v3_run.tcl
@@ -0,0 +1,97 @@
+#setup path for library
+set_db init_lib_search_path /mnt/apps/prebuilt/eda/designkits/GPDK/gsclib045/lan/flow/t1u1/reference_libs/GPDK045/gsclib045_svt_v4.4/gsclib045/timing
+
+#setup script path
+set_db script_search_path Desktop/VLSI/Script/
+
+#setup hdl path
+set_db init_hdl_search_path Desktop/VLSI/Verilog/
+
+#Setup timing library
+set_db library { /mnt/apps/prebuilt/eda/designkits/GPDK/gsclib045/lan/flow/t1u1/reference_libs/GPDK045/gsclib045_svt_v4.4/gsclib045/timing/slow_vdd1v0_basicCells.lib /mnt/apps/prebuilt/eda/designkits/GPDK/gsclib045/lan/flow/t1u1/reference_libs/GPDK045/gsclib045_svt_v4.4/gsclib045/timing/slow_vdd1v0_multibitsDFF.lib }
+#end
+
+#Setup lef library
+set_db lef_library {/mnt/apps/prebuilt/eda/designkits/GPDK/gsclib045/lan/flow/t1u1/reference_libs/GPDK045/gsclib045_svt_v4.4/gsclib045/lef/gsclib045_tech.lef  /mnt/apps/prebuilt/eda/designkits/GPDK/gsclib045/lan/flow/t1u1/reference_libs/GPDK045/gsclib045_svt_v4.4/gsclib045/lef/gsclib045_macro.lef /mnt/apps/prebuilt/eda/designkits/GPDK/gsclib045/lan/flow/t1u1/reference_libs/GPDK045/gsclib045_svt_v4.4/gsclib045/lef/gsclib045_multibitsDFF.lef  }
+#end
+
+#Setup qrc library
+read_qrc /mnt/apps/prebuilt/eda/designkits/GPDK/gsclib045/lan/flow/t1u1/reference_libs/GPDK045/gsclib045_svt_v4.4/gsclib045/qrc/qx/gpdk045.tch
+#end
+
+#use multibits
+set_db use_multibit_cells true
+
+#setup hdl file
+read_hdl picorv32.v
+
+#elaborate
+elaborate "picorv32"
+
+#Verification rtl vs elab step
+write_netlist -lec > elab.v
+write_do_lec -top picorv32 -golden_design rtl -revised_design elab.v -log_file rtl_elab.lec.log > rtl_elab.do
+
+#check design
+check_design -all > Desktop/VLSI/Exercises/Ex7/Step1-7/v3/Checks/Ex7_Step1-7_v3_Check_Design.txt
+
+#read the sdc file
+read_sdc Ex1_Step4_v3.sdc
+
+#check timing intent
+check_timing_intent > Desktop/VLSI/Exercises/Ex7/Step1-7/v3/Checks/Ex7_Step1-7_v3_Check_Timing_Intent.txt
+
+# setting forinnovus
+set_db / .use_scan_seqs_for_non_dft false
+
+#do syn_generic
+syn_generic 
+
+#verification elab vs generic map
+#write_netlist -lec > generic.v
+#write_do_lec -top picorv32 -golden_design elab.v -revised_design generic.v -log_file elab_generic.lec.log > elab_generic.do
+
+#do syn_map
+syn_map
+
+# Verification generic vs fv_map step
+#write_netlist -lec > fv_map.v
+#write_do_lec -top picorv32 -golden_design generic.v -revised_design fv_map.v -log_file generic_fvmap.lec.log > generic_fvmap.do
+
+# Verification generic vs fv_map step
+write_netlist -lec > map.v
+write_do_lec -top picorv32 -golden_design rtl -revised_design map.v -log_file rtl_map.lec.log > rtl_map.do
+
+#do optimation
+syn_opt
+
+#report area
+report_area > Desktop/VLSI/Exercises/Ex7/Step1-7/v3/Reports/Ex7_Step1-7_v3_Area.txt
+
+#report gates
+report_gates > Desktop/VLSI/Exercises/Ex7/Step1-7/v3/Reports/Ex7_Step1-7_v3_Gates.txt
+
+#report timing
+report_timing > Desktop/VLSI/Exercises/Ex7/Step1-7/v3/Reports/Ex7_Step1-7_v3_Timing.txt
+
+#report power
+report_power > Desktop/VLSI/Exercises/Ex7/Step1-7/v3/Reports/Ex7_Step1-7_v3_Power.txt
+
+#report qor
+report_qor > Desktop/VLSI/Exercises/Ex7/Step1-7/v3/Reports/Ex7_Step1-7_v3_Qor.txt
+
+#export design
+#------------- #optional steps
+write_hdl > Desktop/VLSI/Exercises/Ex7/Step1-7/v3/Exported/Ex7_Step1-7_v3_design.v
+write_sdc > Desktop/VLSI/Exercises/Ex7/Step1-7/v3/Exported/Ex7_Step1-7_v3_constraints.sdc
+write_script > Desktop/VLSI/Exercises/Ex7/Step1-7/v3/Exported/Ex7_Step1-7_v3_constraints.g
+
+# this line is for innovus tool and generate a custom folder 
+#write_design -base_name Desktop/VLSI/Exercises/Ex1/Step7/v3/Genus_Innovus_Design/genus/ -innovus picorv32
+#end use of genus no go on innovus
+
+# this line is for innovus tool and generate deafult folder
+write_design -innovus picorv32
+#end use of genus no go on innovus
+
+exit
---
+++ Ex7/genus_invs_des/genus.default_emulate_view_latency.sdc
@@ -0,0 +1,1 @@
+set_clock_latency -source 0.5 [get_clocks clk]
